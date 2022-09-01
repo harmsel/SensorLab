d2fca230fb1f634ab25d15e980f8d3375dfb8885
Harm van Vugt <h.e.m.van.vugt@hva.nl>
2022-09-01 15:41:27 +0200

    Update ServoBasis.ino

--- Basics-components/ServoBasis/ServoBasis.ino
+++ Basics-components/ServoBasis/ServoBasis.ino
@@ -1,25 +1,16 @@
-#include <Servo.h>
-#include "Ultrasonic.h"
-Ultrasonic ultrasonic(7);
-
-Servo myservo;  // create servo object to control a servo
+Servo myservo;  // maakt een servo object aan
 
 void setup() {
-  myservo.attach(8);  // attaches the servo on pin 9 to the servo object
+  myservo.attach(8);  // Sluit de servo aan op D8
   Serial.begin(9600);
 }
 
 void loop() {
-  long RangeInCentimeters; //variabele voor het opslaan van de afstand
-  RangeInCentimeters = ultrasonic.MeasureInCentimeters();
-  Serial.println(RangeInCentimeters);//0~400cm
-  delay(50);// korte tijd tussen metingen
-
-
-  if (RangeInCentimeters < 30) {
-    myservo.write(10);                  // draai de servo naar 10 graden, op 0 graden gaat deze vaak trillen
-    delay(100);                           // 1 seconde wachten
-    myservo.write(40);                  // draai de servo naar 170 graden, op 180 graden gaat soms vaak trillen
-    delay(100);
-  }
+    myservo.write(10); // draai de servo naar 10 graden
+    Serial.println ("Draai naar 170 graden"); // Stuur een string naar de serial Monitor
+    delay(1000);  // 1 seconde wachten
+  
+    myservo.write(170); // draai de servo naar 170 graden,
+    Serial.println ("Draai naar 170 graden");  // Stuur een string naar de serial Monitor
+    delay(1000); // 1 seconde wachten
 }
