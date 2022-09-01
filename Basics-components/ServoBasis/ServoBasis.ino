6bd6f47092cfc89cb21a48f6b008624b750f35c8
Harm van Vugt <h.e.m.van.vugt@hva.nl>
2022-09-01 15:45:46 +0200

    Update ServoBasis.ino

--- Basics-components/ServoBasis/ServoBasis.ino
+++ Basics-components/ServoBasis/ServoBasis.ino
@@ -8,7 +8,7 @@ void setup() {
 
 void loop() {
     myservo.write(10); // draai de servo naar 10 graden
-    Serial.println ("Draai naar 170 graden"); // Stuur een string naar de serial Monitor
+    Serial.println ("Draai naar 10 graden"); // Stuur een string naar de serial Monitor
     delay(1000);  // 1 seconde wachten
   
     myservo.write(170); // draai de servo naar 170 graden,
