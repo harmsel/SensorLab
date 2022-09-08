96396b73dbca54c1b276c63aff409a9e893a59cb
Harm van Vugt <h.e.m.van.vugt@hva.nl>
2022-09-08 16:35:05 +0200

    update

--- Basics-components/ledZonderDelay/ledZonderDelay.ino
+++ Basics-components/ledZonderDelay/ledZonderDelay.ino
@@ -1,19 +1,20 @@
-#include <elapsedMillis.h>
+#include <elapsedMillis.h> //installeer de lapsedMillis.h library (van Peter Freerick)
 elapsedMillis verstrekenTijd1;
+int ledPin = 5;  //Sluit de blauwe LED/KNOP aan op D5
 
 void setup() {
-  pinMode(3, OUTPUT);  //LED
-  pinMode(5, OUTPUT);  //LED
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
 
   if (verstrekenTijd1 == 1000) {  //zolang de teller minder dan 1000 is, doe onderstaande regel:
-    digitalWrite(3, LOW);        // Zet de led aan (bij deze led: LOW = aan, HIGH = uit)
+    digitalWrite(ledPin, LOW);        // Zet de led aan (bij deze led: LOW = aan, HIGH = uit)
 
   } else if (verstrekenTijd1 == 2000) {  // zolang de teller niet onder 1000 wel onder de 2000 is:
-    digitalWrite(3, HIGH);
-    verstrekenTijd = 0;     
+    digitalWrite(ledPin, HIGH);
+    verstrekenTijd1 = 0;
 
- delay(1);// zorg dat de output in Serial monitor beter leeesbaar is   
+    delay(1);// zorg dat de output in Serial monitor beter leeesbaar is
+  }
 }
