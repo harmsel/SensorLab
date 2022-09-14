50545178d88a23deac0ee6c6835abbde9247251b
Harm van Vugt <h.e.m.van.vugt@hva.nl>
2022-09-14 11:34:49 +0200

    Update LED-Acceler.ino

--- Basics-interaction/LED-Acceler/LED-Acceler.ino
+++ Basics-interaction/LED-Acceler/LED-Acceler.ino
@@ -1,29 +1,29 @@
-// installeer de library "ADXL345.h" via Sketch -> Include Library -> Manage library
-#include <Wire.h> // sluit de accelerometer aan op I2C
-#include <ADXL345.h>
-ADXL345 adxl;
+// installeer de library "ADXL345.h" (van Seeed Studio) via "Tools > Manage libraries"
+#include <Wire.h>     // Sluit de accelerometer aan op I2C
+#include <ADXL345.h>  // Neemt de library op in deze sketch
+ADXL345 adxl;         // Maakt een object van de library aan met de naam "adxl". 
 
 
-#include <Adafruit_NeoPixel.h> //installeer de library (als je dat nog niet gedaan had)
-#define PIN 4         // Prik de ledstrip in D4
-#define NUMPIXELS 16  // Aantal pixels in je ledstrip
+#include <Adafruit_NeoPixel.h>  //Deze library had je al eerder geinstalleerd, dus nogmaals installeren is niet nodig 
+#define PIN 4                   // Sluit de ledstrip aan op D4
+#define NUMPIXELS 16            // Dit zijn het aantal pixels in je ledstrip
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   Serial.begin(9600); //noodzakelijk voor het weergeven van debug tekst in de Serial Monitor
-  adxl.powerOn();
-  pixels.begin();  // INITIALIZE NeoPixel strip object (REQUIRED)
+  adxl.powerOn();     // Zet de I2C poort open voor je Accelerometer
+  pixels.begin();     // noodzakelijk voor de aangesloten ledstrip
 }
 
 void loop() {
   int x, y, z;
-  adxl.readXYZ(&x, &y, &z);  //lees the accelerometer waardes en sla ze op in variabelen
+  adxl.readXYZ(&x, &y, &z);  // Leest the accelerometer waardes, sla ze op in variabelen
 
-  Serial.print("x: ");  Serial.println(x);
+  Serial.print("x: ");  Serial.println(x); // Geef alleen de waarde van x weer in de Serial Monitor
 
   int pixNr = (x / 10) + 5;
-  pixels.clear();  // Set all pixel colors to 'off'
-  pixels.setPixelColor(pixNr, pixels.Color(0, 150, 0));
-  pixels.show();  // Send the updated pixel colors to the hardware.
-  delay(20); //voor betere leesbaarheid van de serial monitor
+  pixels.clear();   // Set all pixel colors to 'off'
+  pixels.setPixelColor(pixNr, pixels.Color(0, 150, 0)); // de nummers zijn de R, G, B waarden
+  pixels.show();    // Stuur bovenstaande waarden voor pixels naar de Ledstrip
+  delay(20);        //voor betere leesbaarheid van de serial monitor
 }
