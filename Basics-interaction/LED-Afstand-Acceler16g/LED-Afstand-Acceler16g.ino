03f2834ae5dac36bb2ff2a41159b9b78f7cd2b71
Harm van Vugt <h.e.m.van.vugt@hva.nl>
2022-09-18 11:32:43 +0200

    Code voor ToDo3 - getest

--- Basics-interaction/LED-Afstand-Acceler16g/LED-Afstand-Acceler16g.ino
+++ Basics-interaction/LED-Afstand-Acceler16g/LED-Afstand-Acceler16g.ino
@@ -2,8 +2,8 @@
 // Deze code werkt uitsluitend voor de 16g accelemeter (er is uiteraard ook een code voor de 1.5g sensor beschikbaar)
 
 
-#include <ADXL345.h> // installeer de library "ADXL345.h" via Sketch -> Include Library -> Manage library
-ADXL345 accelemeter; // Sluit de Accelerometer aan op een van de I2C connectoren op je UNO (maakt niet uit welke)
+#include <ADXL345.h>  // installeer de library "ADXL345.h" via Sketch -> Include Library -> Manage library
+ADXL345 accelemeter;  // Sluit de Accelerometer aan op een van de I2C connectoren op je UNO (maakt niet uit welke)
 
 #include "Ultrasonic.h"    // Deze heb je al geinstalleerd (zo niet, zoek op "Grove ultrasonic ranger" by Seeed Studio)
 Ultrasonic ultrasonic(6);  //De ultrasoon sensor aan D6
@@ -25,36 +25,38 @@ void setup() {
   pixels.show();             // Turn OFF all pixels ASAP
   pixels.setBrightness(50);  // Set helderheid  (max = 255)
 
-  accelemeter.powerOn(); // Zet de accelemeter aan, noodzakelijke code
+  accelemeter.powerOn();  // Zet de accelemeter aan, noodzakelijke code
 }
 void loop() {
 
   // -- Code voor het uitlezen van  afstandsensor
   long afstand = ultrasonic.MeasureInCentimeters();
+  Serial.print("afstand: ");
   Serial.println(afstand);
 
   // -- code voor het uitlezen van de accelerometer
   int x, y, z;
   accelemeter.readXYZ(&x, &y, &z);  //lees the accelerometer waardes en sla ze op in variabelen
-  Serial.print("x: ");  Serial.println(x); //stuur de waarde van x naar de serial monitor
-  delay(40);//vertragen zodat de serial monitor leesbaarder is
+  Serial.print("x: ");
+  Serial.println(x);  //stuur de waarde van x naar de serial monitor
+
 
 
   /// --  code zodat de LED regenboog feedback gaat geven als de fles leeg (afstand meer dan 30 cm) is
   if (afstand > 30) {  // we gaan er van uit dat de fles 30cm diep is
-    Serial.println("de fles is leeg");
+    Serial.println("De fles is leeg");
     rainbowCycle(5, 1);  // functie aanroep met waarden: (snelheid, herhalingen)
+  } else {
+    colorWipe(pixels.Color(0, 0, 0), 50);  // Red
   }
 
   // - code waardoor de LED regenboog feedback gaat geven als hij rechtop (x tussen -2 en 2) staat
-  if (x < 20 && x > -20) {  // de fles staat best recht als x tussen -20 en 20 zit
-    rainbowLoopt();       // functie aanroep met waarden: (snelheid, herhalingen)
-    Serial.println("de fles staat recht");
+  if (x < 20 && x > -20) {  // mijn fles staat redelijk recht als x tussen -20 en 20 zit, zijn jouw waarden anders als de fles recht staat, pas de waardes gerust aan
+    Serial.println("De fles staat recht");
   }
-  delay(30);  // om de serial monitor leesbaarder te maken
 
-  if (x < 20 && x > -20) {
-     if (afstand > 30)
+
+  delay(30);  //wacht even elke Loop, om de serial monitor leesbaarder te maken
 }
 
 
@@ -103,4 +105,33 @@ uint32_t Wheel(byte WheelPos) {
   }
   WheelPos -= 170;
   return pixels.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
+
+
+// Rainbow cycle along whole strip. Pass delay time (in ms) between frames.
+void rainbow(int wait) {
+  // Hue of first pixel runs 5 complete loops through the color wheel.
+  // Color wheel has a range of 65536 but it's OK if we roll over, so
+  // just count from 0 to 5*65536. Adding 256 to firstPixelHue each time
+  // means we'll make 5*65536/256 = 1280 passes through this loop:
+  for (long firstPixelHue = 0; firstPixelHue < 1 * 65536; firstPixelHue += 256) {
+    // strip.rainbow() can take a single argument (first pixel hue) or
+    // optionally a few extras: number of rainbow repetitions (default 1),
+    // saturation and value (brightness) (both 0-255, similar to the
+    // ColorHSV() function, default 255), and a true/false flag for whether
+    // to apply gamma correction to provide 'truer' colors (default true).
+    pixels.rainbow(firstPixelHue);
+    // Above line is equivalent to:
+    // strip.rainbow(firstPixelHue, 1, 255, 255, true);
+    pixels.show();  // Update strip with new contents
+    delay(wait);    // Pause for a moment
+  }
+}
+
+void colorWipe(uint32_t color, int wait) {
+  for (int i = 0; i < pixels.numPixels(); i++) {  // For each pixel in strip...
+    pixels.setPixelColor(i, color);               //  Set pixel's color (in RAM)
+    pixels.show();                                //  Update strip to match
+    delay(wait);                                  //  Pause for a moment
+  }
 }
