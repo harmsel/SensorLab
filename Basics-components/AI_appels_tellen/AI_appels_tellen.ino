715a6c139da6137766f12c26530c7f3380f906b2
Harm van Vugt <h.e.m.van.vugt@hva.nl>
2023-06-13 19:32:58 +0200

    Eerste basiscode AI module grove

---
+++ Basics-components/AI_appels_tellen/AI_appels_tellen.ino
@@ -0,0 +1,88 @@
+#include "Seeed_Arduino_GroveAI.h"
+#include <Wire.h>
+
+GroveAI ai(Wire);
+uint8_t state = 0;
+void setup()
+{
+  Wire.begin();
+  Serial.begin(115200);
+  
+   Serial.println("begin");
+ // if (ai.begin(ALGO_OBJECT_DETECTION, MODEL_PRE_INDEX_1)) // Deze regen gebruikt het eerste interne model, die kan mensen herkennen en tellen: MODEL_PRE_INDEX_1
+if (ai.begin(ALGO_OBJECT_DETECTION, MODEL_EXT_INDEX_1)) // Extern object, het eerste externe (dus geupload) model
+
+  {
+    Serial.print("Version: 0x");
+    Serial.println(ai.version(), HEX);
+    Serial.print("ID: 0x");
+    Serial.println( ai.id(), HEX);
+    Serial.print("Algo: ");
+    Serial.println( ai.algo());
+    Serial.print("Model: ");
+    Serial.println(ai.model());
+    Serial.print("Confidence: ");
+    Serial.println(ai.confidence());
+    state = 1;
+  }
+  else
+  {
+    Serial.println("Algo begin failed.");
+  }
+}
+
+void loop()
+{
+  if (state == 1)
+  {
+    uint32_t tick = millis();
+    if (ai.invoke()) // begin invoke
+    {
+      while (1) // wait for invoking finished
+      {
+        CMD_STATE_T ret = ai.state(); 
+        if (ret == CMD_STATE_IDLE)
+        {
+          break;
+        }
+        delay(20);
+      }
+
+     uint8_t len = ai.get_result_len(); // receive how many people detect
+     if(len)
+     {
+       int time1 = millis() - tick; 
+       Serial.print("Time consuming: ");
+       Serial.println(time1);
+       Serial.print("Hoeveeel appels: ");
+       Serial.println(len);
+       object_detection_t data;       //get data
+
+       for (int i = 0; i < len; i++)
+       {
+          Serial.println("result:Apples gevonden");
+          Serial.print("Detecting and calculating: ");
+          Serial.println(i+1);
+          ai.get_result(i, (uint8_t*)&data, sizeof(object_detection_t)); //get result
+  
+          Serial.print("confidence:");
+          Serial.print(data.confidence);
+          Serial.println();
+        }
+     }
+     else
+     {
+       Serial.println("No identification");
+     }
+    }
+    else
+    {
+      delay(1000);
+      Serial.println("Invoke Failed.");
+    }
+  }
+  else
+  {
+    state == 0;
+  }
+}
--- Basics-components/LED-strip-heen/LED-strip-heen.ino
+++ Basics-components/LED-strip-heen/LED-strip-heen.ino
@@ -1,28 +1,51 @@
 #include <Adafruit_NeoPixel.h>
 //Installeer de 'Adafruit_NeoPixel.h' library via je Arduino interface
 
-#define PIN        4 // Steek de ledstrip in D4
-#define NUMPIXELS 13 // aantal ledjes in je strip
+#define PIN 4         // Steek de ledstrip in D4
+#define NUMPIXELS 13  // aantal ledjes in je strip
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-#define DELAYVAL 50 // pauze tussen aan/uitgaan ledjes in miliseconden
+#define DELAYVAL 50  // pauze tussen aan/uitgaan ledjes in miliseconden
 
 void setup() {
-  Serial.begin(9600); //neem deze altijd op in je  code voor debuggen
-  pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
-    pixels.clear(); // Zet alle pixels uit
+  Serial.begin(9600);  //neem deze altijd op in je  code voor debuggen
+  pixels.begin();      // INITIALIZE NeoPixel strip object (REQUIRED)
+  pixels.clear();      // Zet alle pixels uit
 }
 
 void loop() {
-  for (int i = 0; i < NUMPIXELS; i++) { // voor elke led
-    pixels.setPixelColor(i, pixels.Color(0, 100, 0));//255 is de max waarde
-    pixels.show();   // stuur de bovenstaande kleur naar de betreffende led
-    delay(DELAYVAL); // pauze tussen het veranderen van de ledjes
-    Serial.println("welke led staat aan: "); Serial.println(i); //Geeft in de Serial monitor weer welke led aanstaat
+
+
+  for (int i = 0; i < 3; i++) {
+    pixels.setPixelColor(i, pixels.Color(100, 0, 0));  //255 is de max waarde
+    Serial.println(i);
+    pixels.show();
+    delay(500);
   }
-  for (int i = NUMPIXELS; i > 0; i--) { // voor elke led
-    pixels.setPixelColor(i, pixels.Color(100, 0, 0));//255 is de max waarde
-    pixels.show();   // stuur de bovenstaande kleur naar de betreffende led
-    delay(DELAYVAL); // pauze tussen het veranderen van de ledjes
-    Serial.println("welke led staat aan: "); Serial.println(i); //Geeft in de Serial monitor weer welke led aanstaat
+
+
+  for (int i = 2; i < 1; i--) {
+    pixels.setPixelColor(i, pixels.Color(0, 0, 0));  //255 is de max waarde
+    Serial.println(i);
+    pixels.show();
+    delay(500);
   }
 }
+
+//   for (int i = 3; i < 6; i++) {                        // voor elke led
+//     pixels.setPixelColor(i, pixels.Color(100, 0, 0));  //255 is de max waarde
+//   }
+
+
+//   pixels.show();    // stuur de bovenstaande kleur naar de betreffende led
+//   delay(DELAYVAL);  // pauze tussen het veranderen van de ledjes
+//   Serial.println("welke led staat aan: ");
+//   Serial.println(i);  //Geeft in de Serial monitor weer welke led aanstaat
+// }
+// for (int i = NUMPIXELS; i > 0; i--) {                // voor elke led
+//   pixels.setPixelColor(i, pixels.Color(100, 0, 0));  //255 is de max waarde
+//   pixels.show();                                     // stuur de bovenstaande kleur naar de betreffende led
+//   delay(DELAYVAL);                                   // pauze tussen het veranderen van de ledjes
+//   Serial.println("welke led staat aan: ");
+//   Serial.println(i);  //Geeft in de Serial monitor weer welke led aanstaat
+// }
+// }
